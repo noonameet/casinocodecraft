72d7e8b36e82dc6e86cccb648b9dc65ef3c51fcf
JJAM2005 <angulojairo00@gmail.com>
2024-03-11 22:52:46 -0500

    diseño2.0

---
+++ Diseño/src/Casino/Code.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Casino;
+
+import Vista.*;
+
+
+/**
+ *
+ * @author Jairo
+ */
+public class Code {
+    public static void main(String[] args) {
+        Log_in inicio = new Log_in();
+        inicio.setVisible(true);
+    }
+}
