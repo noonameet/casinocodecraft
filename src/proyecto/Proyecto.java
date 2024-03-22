5cd4abfd3d6f6bac19e16ce9a93f97001c3c2608
noonameet <katarinanoonaquawiz@gmail.com>
2024-03-21 21:27:47 -0500

    Update Proyecto.java

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -4,16 +4,12 @@ import Modelo.Reg_Cliente;
 import Vista.Vista;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author Laderson Leon
- */
 public class Proyecto {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         Reg_Cliente cliente = new Reg_Cliente();
         Vista vs = new Vista();
+        
         Controlador ct = new Controlador(vs);
         vs.setResizable(false); 
         vs.setVisible(true);
